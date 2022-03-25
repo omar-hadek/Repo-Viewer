4acf4fea6ec97f7eb3d95c7635f75b0551944ad4
omar-hadek <omar.hadek7@gmail.com>
2022-03-25 14:08:22 +0100

    set up app router

---
+++ lib/core/presentation/routes/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:auto_route/annotations.dart';
+import 'package:github_app/auth/presentation/sign_in_page.dart';
+import 'package:github_app/splash/presentation/splash_page.dart';
+import 'package:github_app/starred_repos/presentation/starred_repos_page.dart';
+
+@MaterialAutoRouter(
+  routes: [
+    MaterialRoute(page: SplashPage, initial: true),
+    MaterialRoute(
+      page: SignInPage,
+      path: '/sign-in',
+    ),
+    MaterialRoute(
+      page: StarredReposPage,
+      path: '/repos',
+    ),
+  ],
+  replaceInRouteName: 'Page,Route',
+)
+class $AppRouter {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:github_app/core/presentation/app_widget.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(ProviderScope(child: AppWidget()));
