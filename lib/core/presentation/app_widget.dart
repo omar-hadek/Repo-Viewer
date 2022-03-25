8718d23d9a7d907a181d5371e7f70f91c7e34ad8
omar-hadek <omar.hadek7@gmail.com>
2022-03-25 23:34:27 +0100

    added and impelement initialization provider

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:github_app/auth/application/auth_notifier.dart';
+import 'package:github_app/auth/shared/auth_providers.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'routes/app_router.gr.dart';
 
-class AppWidget extends StatelessWidget {
+final initializationProvider = FutureProvider((ref) async {
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthStatus();
+});
+
+class AppWidget extends ConsumerWidget {
   final appRouter = AppRouter();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AuthState>(authNotifierProvider, (previous, next) {
+      next.maybeMap(
+        orElse: () {},
+        authenticated: (_) {
+          appRouter.pushAndPopUntil(
+            const StarredReposRoute(),
+            predicate: (route) => false,
+          );
+        },
+        unauthenticated: (_) {
+          appRouter.pushAndPopUntil(
+            const SignInRoute(),
+            predicate: (route) => false,
+          );
+        },
+      );
+    });
     return MaterialApp.router(
       title: 'Github reppos',
       routerDelegate: appRouter.delegate(),
